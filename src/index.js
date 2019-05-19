6ef34ad143b6e8b2caab97e714613a44fcec7989
mor1ins <kuliev.ed@gmail.com>
2019-05-19 19:25:26 +0300

    map added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import Phaser from "phaser";
 import logoImg from "./assets/logo.png";
+import mapImg from "./assets/map1.jpg";
+// import enviroment from "./enviroment.js"
+
+const enviroment = {
+  game_width: 1050,
+  game_height: 700
+};
+
 
 const config = {
   type: Phaser.AUTO,
   parent: "phaser-example",
-  width: 800,
-  height: 600,
+  width: enviroment.game_width,
+  height: enviroment.game_height,
   scene: {
     preload: preload,
     create: create
@@ -15,15 +23,22 @@ const config = {
 const game = new Phaser.Game(config);
 
 function preload() {
+  this.load.image("map", mapImg);
 }
 
 function create() {
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
+  const logo = this.add.image(
+    enviroment.game_width / 2,
+    enviroment.game_height / 2,
+    "map"
+  );
+
+  // this.tweens.add({
+  //   targets: logo,
+  //   y: 450,
+  //   duration: 2000,
+  //   ease: "Power2",
+  //   yoyo: true,
+  //   loop: -1
+  // });
 }
